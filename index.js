c26fff4212e97bb3b68701a5ac5c6c85844a6df7
Haroon Salimi <salimiharoon@gmail.com>
2025-09-08 20:27:28 -0400

    solution complete

--- index.js
+++ index.js
@@ -4,8 +4,18 @@
 // Challenge: Check if Sorted
 // Write a function that returns true if the array is sorted in ascending order, false otherwise.
 
+//Input: arr Output: boolean - true or false 
+
 function checkIfSorted(arr) {
-  // Your code here
+  // loop through the array
+  for (let i = 0; i < arr.length; i++) {
+  // start comparing the current element (#) with the next element (#). Then contiunue comparing every following number with the number before it.  
+  // How we will check for ascending order --> if the current number is greater than the next number, the array is not sorted. Return false
+    if (arr[i] > arr[i+1]) {
+      return false
+    }
+  }
+  return true // If the loop ends, we can confirm its sorted
 }
 
 // --- Starter Test Cases ---
@@ -14,3 +24,5 @@ console.log(checkIfSorted([1, 3, 2, 4])); // false
 
 // --- Your Turn ---
 // Add at least 2 more test cases and explain them out loud in your recording.
+console.log(checkIfSorted([1, 2, 2, 3, 4]));
+console.log(checkIfSorted([4, 3, 2, 1]));
